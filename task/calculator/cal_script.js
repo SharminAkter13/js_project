bfc54ea37c0c4127f0d688ae34bcdb4765e8402d
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-10 12:24:53 +0600

    new

--- task/calculator/cal_script.js
+++ task/calculator/cal_script.js
@@ -2,3 +2,20 @@ function Solve(val){
   var v = document.getElementById("res");
   v.value += val;
 }
+function result(){
+  var num1 = document.getElementById("res").value;
+  try{
+    var num2 = eval(num1.replace("x","*"));
+    document.getElementById("res").value = num2;
+  }catch{
+    document.getElementById("res").value = "Error";
+  }
+}
+function Clear(){
+  var inp = document.getElementById("res");
+  inp.value = "";
+}
+function Back(){
+  var ev =document.getElementById("res");
+  ev.value = ev.value.slice(0,-1);
+}
